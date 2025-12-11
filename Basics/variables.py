f34a4b6d659cf95918646c69501097d6ba4a6a2b
Amit Kumar <15000amitkumar@gmail.com>
2025-12-11 06:57:35 +0530

    first commit

---
+++ Basics/variables.py
@@ -0,0 +1,25 @@
+x = 10
+name = "Amit"
+price = 99.12
+
+print(x)
+print(name)
+print(name,price)
+
+print(f"My name is {name} and price is {price}")
+
+
+#single comment
+
+"""
+    Multi Comment
+    
+"""
+
+print(2+3)
+print(12-45)
+print("Hi" + "Sumit")
+print("Hi" * 3)
+print(-12//5)
+print(float(12)/5)
+
